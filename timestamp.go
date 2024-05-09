b0b7bb9e35a403af46a3a765fae70c5a0cecb97e
semi <semilin@disroot.org>
2024-05-09 16:28:47 -0700

    add timestamp.go

---
+++ timestamp.go
@@ -0,0 +1,22 @@
+package types
+
+import (
+	"encoding/json"
+	"strconv"
+	"time"
+)
+
+type Timestamp time.Time
+
+func (t Timestamp) MarshalJSON() ([]byte, error) {
+	return []byte(strconv.Quote(time.Time(t).Format(time.RFC3339))), nil
+}
+
+func (t *Timestamp) UnmarshalJSON(b []byte) error {
+	var timestamp int64
+	if err := json.Unmarshal(b, &timestamp); err != nil {
+		return err
+	}
+	*t = Timestamp(time.Unix(timestamp, 0))
+	return nil
+}
